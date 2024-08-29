12e88779aaa30174494949e1817a043477151dc6
Nijeesh Joshy <nijeesh.k@rently.com>
2024-08-29 17:39:41 +0530

    seperated out private class methods

--- lib/rubypostal.rb
+++ lib/rubypostal.rb
@@ -1,26 +1,45 @@
+# frozen_string_literal: true
 require 'ffi'
-require 'thread'
-
 require_relative 'rubypostal/libpostal_wrapper'
 
+# The main module for the gem
 module Rubypostal
   extend LibpostalWrapper
 
   def self.parse_address(address)
-    options = libpostal_get_address_parser_default_options()
-    response = libpostal_parse_address(address, options)
-
-    results = {}
-    num_components = response[:num_components]
-
-    num_components.times do |i|
-      label = response[:labels] + i * FFI::Pointer.size
-      component = response[:components] + i * FFI::Pointer.size
-      results[label.read_pointer.read_string] = component.read_pointer.read_string
-    end
-
-    libpostal_address_parser_response_destroy(response)
+    options = get_default_options
+    response = parse_address_with_options(address, options)
+    results = extract_results(response)
+    destroy_response(response)
 
     results
+  end
+
+  class << self
+
+    def self.get_default_options
+      libpostal_get_address_parser_default_options()
+    end
+
+    def self.parse_address_with_options(address, options)
+      libpostal_parse_address(address, options)
+    end
+
+    def self.extract_results(response)
+      results = {}
+      num_components = response[:num_components]
+
+      num_components.times do |i|
+        label = response[:labels] + i * FFI::Pointer.size
+        component = response[:components] + i * FFI::Pointer.size
+        results[label.read_pointer.read_string] = component.read_pointer.read_string
+      end
+
+      results
+    end
+
+    def self.destroy_response(response)
+      libpostal_address_parser_response_destroy(response)
+    end
   end
 end
